4b936d26af8e0fbc31794723ec8e3f3dca513cff
tonitdiep <adiep123@gmail.com>
2021-03-04 04:46:27 -0500

    commit tree 76f01f948e1814bb81c1a5679d3c431ebecb5d0e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("#main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion!";
+newHeader.className = "victory";
